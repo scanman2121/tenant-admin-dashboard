c748db17ce21a470261f71787b20f227144eb339
Your Name <you@example.com>
2025-03-21 16:15:58 -0400

    fix: remove duplicate header from visitors page

--- src/app/(main)/visitors/page.tsx
+++ src/app/(main)/visitors/page.tsx
@@ -29,24 +29,19 @@ export default function Visitors() {
                     </Button>
                 }
             />
-            <div className="p-4 sm:px-6 sm:pb-10 sm:pt-10 lg:px-10 lg:pt-7">
-                <h1 className="text-[24px] font-medium text-gray-900 dark:text-gray-50">
-                    Visitors
-                </h1>
-                <TabNavigation className="mt-4 sm:mt-6 lg:mt-10">
-                    {tabs.map((tab) => (
-                        <TabNavigationLink
-                            key={tab.name}
-                            asChild
-                            active={pathname === tab.href}
-                        >
-                            <Link href={tab.href}>{tab.name}</Link>
-                        </TabNavigationLink>
-                    ))}
-                </TabNavigation>
-                <div className="mt-8">
-                    <DataTable data={visitors} columns={visitorColumns} />
-                </div>
+            <TabNavigation className="px-4 sm:px-6 lg:px-10">
+                {tabs.map((tab) => (
+                    <TabNavigationLink
+                        key={tab.name}
+                        asChild
+                        active={pathname === tab.href}
+                    >
+                        <Link href={tab.href}>{tab.name}</Link>
+                    </TabNavigationLink>
+                ))}
+            </TabNavigation>
+            <div className="px-4 sm:px-6 lg:px-10">
+                <DataTable data={visitors} columns={visitorColumns} />
             </div>
         </div>
     )
